cf6d950fc5627765484da5be0f9af2d04e8f938f
me-marchi <me.marchi@outlook.com>
2022-02-18 12:03:34 -0300

    projeto criado

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+//
+
+const app = express()
+//a função express() retorna uma aplicação express
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/pages/home.html'))
+})
+//puxa o caminho do arquivo home
+
+router.get('/contato', (req, res) => {
+    res.sendFile(path.join(__dirname + '/pages/contato.html'))
+})
+//estabelecendo as rotas para acessar o arquivo html
+
+app.use(router)
+//diz que a aplicação está utilizando estas rotas
+
+app.listen(3333, () => {
+    console.log('Servidor rodando')
+})
